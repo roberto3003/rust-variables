3451aa48c4160455191eea84c4f1f719d0cee259
bobby3003 <roberto3003@riseup.net>
2021-09-18 19:15:04 +0700

    two functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+#![allow(unused)]
+
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+
+    println!("The value of x is: {}", x);
+
+    x = 6;
+
+    println!("The value of x is: {}", x);
+
+    const MAX_POINTS: u32 = 100_000;
+
+    println!("The max value for this game is: {}", MAX_POINTS);
 }
