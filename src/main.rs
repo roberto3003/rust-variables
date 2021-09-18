293859ab6b4551a52552d114fbbc94a6c88d573d
bobby3003 <roberto3003@riseup.net>
2021-09-18 19:21:51 +0700

    finished

--- src/main.rs
+++ src/main.rs
@@ -12,4 +12,10 @@ fn main() {
     const MAX_POINTS: u32 = 100_000;
 
     println!("The max value for this game is: {}", MAX_POINTS);
+
+    let x = x + 1;
+
+    let x = x * 2;
+
+    println!("The value of x is: {}", x);
 }
